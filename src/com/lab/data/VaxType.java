42b97bee97f0e9b71b676db71832510dabfaf132
Gallinator <lcgallinator4@gmail.com>
2021-05-09 16:36:22 +0200

    VaxType: initial commit

---
+++ src/com/lab/data/VaxType.java
@@ -0,0 +1,36 @@
+package com.lab.data;
+
+/**
+ * This enumerator represents a vaccination type.
+ * toString() methods are overridden to easily get properly formatted names.
+ *
+ * @author Cicer Luigi
+ */
+
+public enum VaxType {
+
+    PFIZER {
+        @Override
+        public String toString() {
+            return "Pfizer";
+        }
+    },
+    ASTRAZENECA {
+        @Override
+        public String toString() {
+            return "AstraZeneca";
+        }
+    },
+    MODERNA {
+        @Override
+        public String toString() {
+            return "Moderna";
+        }
+    },
+    JJ {
+        @Override
+        public String toString() {
+            return "Johnson & Johnson";
+        }
+    }
+}
