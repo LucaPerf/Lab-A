0c7f45976a57aabcdce9927707507bf3806de387
Gallinator <lcgallinator4@gmail.com>
2021-07-21 11:33:44 +0200

    Cittadini: initial commit

---
+++ src/main/java/com/lab/cittadini/Cittadini.java
@@ -0,0 +1,10 @@
+package com.lab.cittadini;
+
+import com.lab.data.User;
+
+public class Cittadini {
+    public static void registraCittadino(User user)
+    {
+        //Registrati.add(user);
+    }
+}
