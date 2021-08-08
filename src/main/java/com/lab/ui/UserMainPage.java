1af6d27cdd231d52c79359a3aec60162a4c6ea47
Gallinator <lcgallinator4@gmail.com>
2021-08-08 12:11:22 +0200

    UserMainPage: improve search UI

--- src/main/java/com/lab/ui/UserMainPage.java
+++ src/main/java/com/lab/ui/UserMainPage.java
@@ -1,15 +1,20 @@
 package com.lab.ui;
 
-import com.jfoenix.controls.JFXButton;
-import com.jfoenix.controls.JFXListView;
-import com.jfoenix.controls.JFXTextField;
+import com.jfoenix.controls.*;
+import com.lab.cittadini.Cittadini;
+import com.lab.data.Center;
+import com.lab.data.CenterType;
 import com.lab.data.User;
+import com.lab.datamanager.Centri;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+
+import java.util.LinkedList;
 
 /**
  * Controller of the user area main page. Layout is stored in "user_main.fxml".
@@ -32,9 +37,23 @@ public class UserMainPage extends Page {
     @FXML
     private JFXButton register;
     @FXML
-    private JFXTextField searchbar;
+    private TextField searchbar;
     @FXML
-    private JFXListView centers;
+    private JFXButton searchButton;
+    @FXML
+    private JFXListView<Center> centers;
+    @FXML
+    private HBox typeFilters;
+    @FXML
+    private JFXToggleNode hub;
+    @FXML
+    private JFXToggleNode ospedaliero;
+    @FXML
+    private JFXToggleNode aziendale;
+    @FXML
+    private JFXToggleNode nameFilter;
+    @FXML
+    private JFXToggleNode comuneTypeFilter;
 
     private User currentUser;
 
@@ -51,16 +70,30 @@ public class UserMainPage extends Page {
      */
     @Override
     protected void initialize() {
-        logout.setVisible(false);
-        username.setVisible(false);
+        reset();
+        setupCentersList();
+
+        //Set up filters
+        nameFilter.setOnAction(actionEvent -> setSearchTypeUI(true));
+        comuneTypeFilter.setOnAction(event -> setSearchTypeUI(false));
+        aziendale.setOnAction(event -> setTypeFiltersUI(CenterType.AZIENDALE));
+        hub.setOnAction(event -> setTypeFiltersUI(CenterType.HUB));
+        ospedaliero.setOnAction(event -> setTypeFiltersUI(CenterType.OSPEDALIERO));
+
+        //Set up buttons
+        searchButton.setOnAction(event -> search());
+        searchbar.setOnKeyPressed(event -> {
+            if (event.getCode().equals(KeyCode.ENTER))
+                search();
+        });
 
         logout.setOnAction(actionEvent -> setLoggedOut());
-
         login.setOnAction(actionEvent -> PagesManager.openUserLogin());
-
         register.setOnAction(actionEvent -> PagesManager.openUserReg());
-
-        back.setOnAction(actionEvent -> PagesManager.openAreaSelection());
+        back.setOnAction(actionEvent -> {
+            PagesManager.openAreaSelection();
+            setLoggedOut();
+        });
     }
 
     /**
@@ -69,7 +102,16 @@ public class UserMainPage extends Page {
     @Override
     public void reset() {
         currentUser = null;
+        logout.setVisible(false);
+        username.setVisible(false);
         username.setText("");
+        nameFilter.setSelected(true);
+        aziendale.setSelected(false);
+        hub.setSelected(false);
+        ospedaliero.setSelected(true);
+        comuneTypeFilter.setSelected(false);
+        typeFilters.setVisible(false);
+        searchbar.clear();
     }
 
     /**
@@ -103,5 +145,67 @@ public class UserMainPage extends Page {
 
         register.setVisible(true);
         login.setVisible(true);
+    }
+
+    /**
+     * Adds 25 centers to the search result list.
+     */
+    public void setupCentersList() {
+        centers.getItems().clear();
+        int i = 0;
+        for (LinkedList<Center> list : Centri.getCenters().values())
+            for (Center c : list) {
+                if (i < 25) {
+                    centers.getItems().add(c);
+                    i++;
+                } else return;
+            }
+    }
+
+    private CenterType getTypeFromUI() {
+        if (nameFilter.isSelected())
+            return null;
+        else if (ospedaliero.isSelected())
+            return CenterType.OSPEDALIERO;
+        else if (aziendale.isSelected())
+            return CenterType.AZIENDALE;
+        else
+            return CenterType.HUB;
+    }
+
+    private void search() {
+        Cittadini.cercaCentroVaccinale(searchbar.getText(), getTypeFromUI());
+    }
+
+    //Hides or shows the UI type filter buttons
+    private void setTypeFiltersUI(CenterType type) {
+        switch (type) {
+            case OSPEDALIERO: {
+                hub.setSelected(false);
+                aziendale.setSelected(false);
+                break;
+            }
+            case AZIENDALE: {
+                hub.setSelected(false);
+                ospedaliero.setSelected(false);
+                break;
+            }
+            case HUB: {
+                aziendale.setSelected(false);
+                ospedaliero.setSelected(false);
+                break;
+            }
+        }
+    }
+
+    //Hides or shoes the UI search type buttons
+    private void setSearchTypeUI(boolean byName) {
+        if (byName) {
+            typeFilters.setVisible(false);
+            comuneTypeFilter.setSelected(false);
+        } else {
+            nameFilter.setSelected(false);
+            typeFilters.setVisible(true);
+        }
     }
 }
