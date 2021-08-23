87470d9fa86bb7f9b058dd3b7ab068f6ea922cf0
Gallinator <lcgallinator4@gmail.com>
2021-08-23 12:26:25 +0200

    Cittadini: sort results by center name

--- src/main/java/com/lab/cittadini/Cittadini.java
+++ src/main/java/com/lab/cittadini/Cittadini.java
@@ -5,6 +5,7 @@ import com.lab.datamanager.Centri;
 import com.lab.datamanager.Registrati;
 import com.lab.datamanager.Vaccinati;
 
+import java.util.Comparator;
 import java.util.LinkedList;
 
 /**
@@ -13,6 +14,8 @@ import java.util.LinkedList;
  * @author Ciceri Luigi
  */
 public class Cittadini {
+
+    private static Comparator<Center> centerNameComp = Comparator.comparing(Center::getName);
 
     /**
      * Add a new user and save its data into "Cittadini_Registrati.csv"
@@ -35,26 +38,31 @@ public class Cittadini {
 
     /**
      * Searches for a center by name or by "comune" and type. If <code>type<code/> is null <code>key</code> is the name of the center, otherwise it is the name of the "comune".
+     * The returned list is sorted in alphabetic order.
      *
      * @param key  The name of the center or "comune" to search for
      * @param type The center type to search for
      */
     public static LinkedList<Center> cercaCentroVaccinale(String key, CenterType type) {
+        LinkedList<Center> list;
         if (type == null)
-            return Centri.find(key);
+            list = Centri.find(key);
         else
-            return Centri.find(type, key);
+            list = Centri.find(type, key);
+
+        list.sort(centerNameComp);
+        return list;
     }
 
     /**
      * Check if the event hash been added in the center file
      *
-     * @param uID Unique id associated with a vaccinated person
-     * @param event Adverse events to be added or have already been added in the file
+     * @param uID        Unique id associated with a vaccinated person
+     * @param event      Adverse events to be added or have already been added in the file
      * @param centerName The name of the center to add its data to the file
      * @return
      */
-    public static boolean inserisciEventiAvversi(Integer uID, Event event, String centerName){
+    public static boolean inserisciEventiAvversi(Integer uID, Event event, String centerName) {
         Vaccinati.load(centerName);
 
         VaxInfo vi = Vaccinati.find(uID);
